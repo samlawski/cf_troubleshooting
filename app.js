ea977bf65796a39cc14afd47ea574dfefdc0aa75
sam <sam.bogu@gmail.com>
2015-11-17 20:39:31 +0100

    changed whole setup to display posts
    removed all faqs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('app', ['ngMaterial', 'ngSanitize']).config(
+  var app = angular.module('app', ['ngMaterial', 'ngSanitize', 'ng-showdown']).config(
     function($mdThemingProvider){
       $mdThemingProvider.theme('default')
         .primaryPalette('cyan')
@@ -8,10 +8,22 @@
   );
 
   //Initialize the sidebar
-  app.controller('faqCtrl', ['$scope', '$http', '$location', '$anchorScroll', function($scope, $http, $location, $anchorScroll){
-    // access faq
-    $http.get('faq.json').success(function(data){
-      $scope.faq = data;
+  app.controller('faqCtrl', ['$scope', '$http', '$location', '$anchorScroll', '$showdown', function($scope, $http, $location, $anchorScroll, $showdown){
+
+    //empty array will contain all the actual posts
+    $scope.all_posts = [];
+
+    //get the file that contains the information about all the posts
+    $http.get('posts.json').success(function(data){
+      $scope.posts = data; //save the data to scope
+
+      //for each post: push post to the scope array of all posts
+      for(i=0;i<$scope.posts.length;++i){
+        $http.get("posts/" + $scope.posts[i].date + $scope.posts[i].number + "_" + $scope.posts[i].name + ".md")
+        .then(function(post){
+          $scope.all_posts.unshift(post.data);//unshift = reversed push
+        });
+      };
     });
 
     //link to answers:
@@ -23,15 +35,15 @@
     };
 
     //show subheaders
-    $scope.previousModule = 0;
+    // $scope.previousModule = 0;
+    //
+    // $scope.showModule = function(module){
+    //   showHeader = (module!=$scope.previousModule);
+    //   //console.log(showHeader);
+    //   $scope.previousModule = module;
+    //   return showHeader;
+    // };
 
-    $scope.showModule = function(module){
-      showHeader = (module!=$scope.previousModule);
-      //console.log(showHeader);
-      $scope.previousModule = module;
-      return showHeader;
-    };
+  }]); //controller
 
-  }]);
-
-})();
+})(); // app
