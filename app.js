0d4d39b92f1263ac11518b0fe35ac317038bbcc5
sam <sam.bogu@gmail.com>
2015-11-18 00:01:27 +0100

    radically simplified code

--- app.js
+++ app.js
@@ -10,37 +10,27 @@
   //Initialize the sidebar
   app.controller('faqCtrl', ['$scope', '$http', '$location', '$anchorScroll', '$showdown', function($scope, $http, $location, $anchorScroll, $showdown){
 
-    //empty array will contain all the actual posts
-    $scope.all_posts = [];
-
     //get the file that contains the information about all the posts
     $http.get('posts.json').success(function(data){
       $scope.posts = data; //save the data to scope
 
       $scope.posts.forEach(function(post){
-        //add slug to posts JSON
+        //add slug property to post:
         post.slug = post.name.replace(/\s+/g, '-').toLowerCase();
 
+        //add markdown file as article property:
         $http.get("posts/" + post.date.replace(/-/g, "") + post.number + "-" + post.slug + ".md")
         .success(function(p){
-          $scope.all_posts.unshift({
-            post: p,
-            date: post.date,
-            name: post.name,
-            slug: post.slug
-          });//unshift = reversed push
+          post.article = p;
         });//sucess
       });//loop
     });//http get
 
-    //link to answers:
+    //link to articles:
     $scope.scroll = function(id){
       $location.hash(id);
       $anchorScroll();
-      //reset url
-      //$location.hash("");
     };
 
   }]); //controller
-
 })(); // app
