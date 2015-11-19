58068e4c3e0166371b9a9717fa3bd9d7789cbf74
sam <sam.bogu@gmail.com>
2015-11-19 10:17:40 +0100

    refactoring and removing deprecated code

--- app.js
+++ app.js
@@ -7,25 +7,27 @@
     }
   );
 
-  //Initialize the sidebar
+  app.factory('Posts', ['$http', '$showdown', function($http, $showdown){
+    
+  }]);
+
   app.controller('faqCtrl', ['$scope', '$http', '$location', '$anchorScroll', '$showdown', function($scope, $http, $location, $anchorScroll, $showdown){
 
-    //get the file that contains the information about all the posts
-    $http.get('posts.json').success(function(data){
-      $scope.posts = data; //save the data to scope
+    // get the file that contains the information about all the posts
+    $http.get('posts.json').then(function(posts){
+      $scope.posts = posts.data; //with '.then' 'posts' is an object
 
       $scope.posts.forEach(function(post){
-        //add slug property to post:
+        // add slug property to post:
         post.slug = post.name.replace(/\s+/g, '-').toLowerCase();
         post.path = "posts/" + post.date.replace(/-/g, "") + post.number + "-" + post.slug + ".md";
 
-        //add markdown file as article property:
-        $http.get(post.path)
-        .success(function(p){
-          post.article = p;
-        });//sucess
-      });//loop
-    });//http get
+        // add markdown file as article property:
+        $http.get(post.path).then(function(p){
+          post.article = p.data;
+        });// :end http get
+      });// :end forEach
+    });// :end http get
 
     //link to articles:
     $scope.scroll = function(id){
@@ -35,5 +37,5 @@
       $location.hash("");
     };
 
-  }]); //controller
-})(); // app
+  }]); // :end controller
+})(); // :end app
