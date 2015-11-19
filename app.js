88dba6e57ae069b595a3b3bcb41a4ed50f00b670
sam <sam.bogu@gmail.com>
2015-11-19 10:20:54 +0100

    refactoring and comment clean up

--- app.js
+++ app.js
@@ -5,17 +5,12 @@
         .primaryPalette('lime')
         .accentPalette('blue-grey');
     }
-  );
-
-  app.factory('Posts', ['$http', '$showdown', function($http, $showdown){
-    
-  }]);
+  ); // :end angular.module
 
   app.controller('faqCtrl', ['$scope', '$http', '$location', '$anchorScroll', '$showdown', function($scope, $http, $location, $anchorScroll, $showdown){
-
-    // get the file that contains the information about all the posts
+    // == get the file that contains the information about all the posts:
     $http.get('posts.json').then(function(posts){
-      $scope.posts = posts.data; //with '.then' 'posts' is an object
+      $scope.posts = posts.data; // :with '.then' 'posts' is an object
 
       $scope.posts.forEach(function(post){
         // add slug property to post:
@@ -25,17 +20,16 @@
         // add markdown file as article property:
         $http.get(post.path).then(function(p){
           post.article = p.data;
-        });// :end http get
-      });// :end forEach
-    });// :end http get
+        }); // :end http get
+      }); // :end forEach
+    }); // :end http get
 
-    //link to articles:
+    // == link to articles:
     $scope.scroll = function(id){
       $location.hash(id);
       $anchorScroll();
-      // reset url:
       $location.hash("");
-    };
+    }; // :end $scope.scroll
 
   }]); // :end controller
-})(); // :end app
+})(); // :end (function)
